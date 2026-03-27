da05e839f89c53bbe4d62e71d431273ea6ac44f3
prasanna7codes <prasannalit098@gmail.com>
2026-03-27 23:46:45 +0530

    adding the many to many allocation in user seller

--- src/models/UserSellerAssignment.js
+++ src/models/UserSellerAssignment.js
@@ -3,10 +3,13 @@ import mongoose from 'mongoose';
 const UserSellerAssignmentSchema = new mongoose.Schema(
     {
         user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-        seller: { type: mongoose.Schema.Types.ObjectId, ref: 'Seller', required: true, unique: true }, // One seller -> one user
+        seller: { type: mongoose.Schema.Types.ObjectId, ref: 'Seller', required: true }, // Many-to-many: one seller can be assigned to multiple users
         dailyTarget: { type: Number, default: 0 } // Daily target set by HR/Superadmin
     },
     { timestamps: true }
 );
+
+// Compound unique index: prevent the same user-seller pair from being assigned twice
+UserSellerAssignmentSchema.index({ user: 1, seller: 1 }, { unique: true });
 
 export default mongoose.model('UserSellerAssignment', UserSellerAssignmentSchema);
