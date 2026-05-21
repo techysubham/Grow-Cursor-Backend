cfcaa0a0cad156863826cab30bbed9c6d28eb3f4
meezumi <aaryank3000@gmail.com>
2026-05-21 10:38:23 +0530

    feat: include product information in auto-fill data generation

--- src/utils/asinAutofill.js
+++ src/utils/asinAutofill.js
@@ -95,7 +95,13 @@ export async function applyFieldConfigs(amazonData, fieldConfigs, pricingConfig 
     brand: amazonData.brand,
     description: amazonData.description,
     price: amazonData.price,
-    asin: amazonData.asin
+    asin: amazonData.asin,
+    product_information: amazonData.productInfo
+      ? Object.entries(amazonData.productInfo)
+          .filter(([, v]) => v !== null && v !== '' && v !== undefined)
+          .map(([k, v]) => `${k}: ${Array.isArray(v) ? v.join(', ') : v}`)
+          .join('\n')
+      : ''
   };
 
   // Images are already an array (same as PAAPI format)
